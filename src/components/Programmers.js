94e903332dec5669d9b651e05e5e09cf531c32a5
Brian Kubes <briankubes@gmail.com>
2020-09-08 13:47:52 -0700

    it works

--- src/components/Programmers.js
+++ src/components/Programmers.js
@@ -36,11 +36,8 @@ export default function Programmers() {
     // It's going to utilize both slices of state to return the _name_ of the featured dev.
     // The beauty of closures is that we can "see" both slices of state from this region
     // of the program, without needing to inject the information through arguments.
-    // return progList[0];
-    // return progList[featured];
-    return progList[0].name;
-    // return progList[featured].name;
-    // return featured;
+
+    return progList[featured - 1].name;
   };
 
   const style = {
